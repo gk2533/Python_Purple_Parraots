8587a53366a77e5b19cf711527868241a620174e
becksonstein <bs3153@columbia.edu>
2018-08-01 15:48:58 -0400

    Merge branch 'development' of https://github.com/bmazey/python_purple_parrots into beck-yodify
    # Conflicts:
    #	application.py
    #	requirements.txt

--- test.py
+++ test.py
@@ -2,47 +2,48 @@ import nltk.corpus
 import nltk.tag
 import nltk
 import re
-
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
-def yodify(s):
-   h = nltk.word_tokenize(s)
-   b = nltk.pos_tag(h)
+class yodify():
 
-   l = len(b)
-   # return b[-2]+ b[-1] + tuple(b[l:l-3]+ b[:l-3])
-   # print(b)
+    def yodify(s):
+       h = nltk.word_tokenize(s)
+       b = nltk.pos_tag(h)
 
-   for item in b:
-       if item[1]== 'PP' :
-           word = re.search(item[0],s)
-           # print(word.start())
-           num = word.start()
-           return s[num:]+ " " +s[:num]
-       if len(b) <= 4:
-           return ' '.join(h[-1:] + h[:len(b)-1])
+       l = len(b)
+       # return b[-2]+ b[-1] + tuple(b[l:l-3]+ b[:l-3])
+       # print(b)
 
-       else:
-           return ' '.join(h[-3:] + h[:-3])
-            # return h[-3:] + h[:-3]
-           # return h[-3:] + " " + h[-2:] + " " + h[-1:]+ " " + str(h[l:l - 3]) + " " + str(h[:l - 3])
+       for item in b:
+           if item[1]== 'PP' :
+               word = re.search(item[0],s)
+               # print(word.start())
+               num = word.start()
+               return s[num:]+ " " +s[:num]
+           if len(b) <= 4:
+               return ' '.join(h[-1:] + h[:len(b)-1])
 
-
-s= "the dog ate the food bowl"
-y = "I like to go to the beach"
-i = "He spoke to Beck and Evan"
-j = "the dog is in the house"
-r = "The quick brown fox jumps over the lazy dog"
-g = "When I was little I had a car door slammed shut on my hand"
-u = "I like dogs"
-x = "My name is Beck"
+           else:
+               return ' '.join(h[-3:] + h[:-3])
+                # return h[-3:] + h[:-3]
+               # return h[-3:] + " " + h[-2:] + " " + h[-1:]+ " " + str(h[l:l - 3]) + " " + str(h[:l - 3])
 
 
+    s= "the dog ate the food bowl"
+    y = "I like to go to the beach"
+    i = "He spoke to Beck and Evan"
+    j = "the dog is in the house"
+    r = "The quick brown fox jumps over the lazy dog"
+    g = "When I was little I had a car door slammed shut on my hand"
+    u = "I like dogs"
+    x = "My name is Beck"
 
-print(yodify(i))
-print(yodify(y))
-print(yodify(j))
-print(yodify(s))
-print(yodify(r))
-print(yodify(u))
-print(yodify(x))
+
+
+    print(yodify(i))
+    print(yodify(y))
+    print(yodify(j))
+    print(yodify(s))
+    print(yodify(r))
+    print(yodify(u))
+    print(yodify(x))
