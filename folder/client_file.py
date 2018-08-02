7849185ef8f8009cfff30986e22d0557ac340598
gabriel_klass <gk2533@columbia.edu>
2018-08-02 15:25:11 -0400

    added 's' to world

---
+++ folder/client_file.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+class Client_Class:
+    def __init__(self):
+        return
+    @staticmethod
+
+    def get_client():
+        j = requests.get('http://pythonredjaguars-env.hx2tdpc8dz.us-east-2.elasticbeanstalk.com/api/recipe/Roasted Asparagus')
+        h = requests.get('http://pythonredjaguars-env.hx2tdpc8dz.us-east-2.elasticbeanstalk.com/api/recipe/Big Night Pizza')
+        r = requests.get('http://pythonredjaguars-env.hx2tdpc8dz.us-east-2.elasticbeanstalk.com/api/recipe/Crock Pot Roast')
+        h = json.loads(h.text)
+        j = json.loads(j.text)
+        r = json.loads(r.text)
+        print(h)
+        print(j)
+        print(r)
+        return h,j,r
---
+++ folder/client_server_file_2.py
@@ -0,0 +1,10 @@
+from folder.client_file import Client_Class
+
+def main():
+
+    return Client_Class.get_client()
+
+
+
+if __name__ == '__main__':
+    main()
